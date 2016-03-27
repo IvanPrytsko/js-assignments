fa6d28fe93cec6a8d7b34b9c0f0cf09daca58a4b
Ivan Prytsko <seekafter.pritko@gmail.com>
2016-03-27 13:06:45 +0300

    2

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -55,7 +55,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    throw new Error('Not implemented');
+    return `Hello, ${firstName} ${lastName}!`
 }
 
 /**
@@ -69,7 +69,8 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    throw new Error('Not implemented');
+    // return value.split(', ')[1].split('!')[0];
+    return value.split(/, |!/)[1]; //split by multiple chars
 }
 
 
